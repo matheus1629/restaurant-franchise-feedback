2275e98e970c549607089dc7bc8c94d47601377f
Matheus Palermo <matheus.palermo@dbccompany.com.br>
2024-03-22 17:12:05 -0300

    CB

--- getewayserver/src/main/java/com/restaurant/getewayserver/GetewayserverApplication.java
+++ getewayserver/src/main/java/com/restaurant/getewayserver/GetewayserverApplication.java
@@ -1,18 +1,11 @@
 package com.restaurant.getewayserver;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -26,25 +19,14 @@ public class GetewayserverApplication {
     public RouteLocator restaurantFeedbackRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(p -> p
-                        .path("/restaurant/feedback/**")
-                        .filters(f -> f.rewritePath("eazybank/accounts/(?<segment>.*)", "/${segment}")
+                        .path("/restaurant/feedbacks/**")
+                        .filters(f -> f.rewritePath("restaurant/feedbacks/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .retry(retryConfig -> retryConfig.setRetries(3)
-                                        .setMethods(HttpMethod.GET)
-                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
-                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
-                                        .setFallbackUri("forward:/contactSupport"))
-                        )
+                                .circuitBreaker(config -> config.setName("feedbacksCollectorCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://FEEDBACKSCOLLECTOR")).build();
 
 
-    }
-
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }
 
 
